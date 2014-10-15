de925ccfad140e0e6efb27ca3cb1d1597cadf0a0
Felix <nolanfyh@gmail.com>
2014-10-15 23:41:01 +0800

    v 0.3.2
    removed pmWordCloud function

--- R/plotWordCloud.R
+++ R/plotWordCloud.R
@@ -9,13 +9,12 @@
 #' @param use.r.layout if false, then c++ code is used for collision detection, otherwise R is used
 #' @param colors color words from least to most frequent
 #' @details This function just call 'wordcloud' from package {wordcloud}. See package {wordcloud} for more details about the parameters.
-#' @seealso \code{\link{pmWordCloud}}
 #' @export
 #' @examples
 #' # text="Jobs received a number of honors and public recognition." 
 #' # cleanD=cleanAbstracts(text)
 #' # plotWordCloud(cleanD,min.freq=1,scale=c(2,1))
-plotWordCloud <- function(abs, scale=c(10,0.2),min.freq=1,max.words=100,
+plotWordCloud <- function(abs, scale=c(3,0.3),min.freq=1,max.words=100,
                           random.order=FALSE,rot.per=0.35,use.r.layout=FALSE, 
                           colors=brewer.pal(8,"Dark2")){
   abs$word=as.character(abs$word)
